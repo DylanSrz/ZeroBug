0516b677c8af188c27fe0f46344ec7c64c7ef838
Dylan Suárez <dylansrz96@gmail.com>
2026-09-20 21:10:07 -0500

    test: e2e de productos contra postgresql y ejecucion en serie de los specs e2e

--- vitest.config.e2e.ts
+++ vitest.config.e2e.ts
@@ -7,5 +7,8 @@ export default defineConfig({
     globals: true,
     root: './',
     include: ['**/*.e2e-spec.ts'],
+    // Los specs que importan AppModule comparten una única PostgreSQL:
+    // se ejecutan en serie para evitar carreras en migraciones y datos.
+    fileParallelism: false,
   },
 });
